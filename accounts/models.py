ba1baea103ad88605965174d19144f03a5ab99c0
Jakub Dulas <jakubdulas.mail@gmail.com>
2020-09-03 22:41:26 +0200

    models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    #profile_pic = models.ImageField()
+    bio = models.TextField(blank=True)
+    #following = models.ManyToManyField(User, blank=True,  related_name='following')
+
+    def __str__(self):
+        return self.user.username
+
+    @property
+    def total_following(self):
+        return self.following.all().count()
+
+    def following_posts(self):
+        return self.post_set.all()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    body = models.TextField(null=True, blank=True)
+    #likes = models.ManyToManyField(Profile, related_name='likes', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.author)
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    
+
+    context = {}
+    return render(request, 'home.html', context)
 
 def profile(request):
     return render(request, 'profile.html')
